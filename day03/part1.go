e8e1cb537dd82f8fe5554208140ea82d2e8ea345
oskar117 <olek.konieczny@wp.pl>
2022-12-03 12:23:03 +0100

    day03

---
+++ day03/part1.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+	"unicode"
+)
+
+func main() {
+	file_name := os.Args[1]
+	file, err := os.Open(file_name)
+
+	if err != nil {
+		fmt.Println("Error opening file!")
+	}
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+
+    result := 0
+
+	for scanner.Scan() {
+		text := scanner.Text()
+		length := len(text)
+		pivot := length / 2
+		left := text[:pivot]
+		right := text[pivot:length]
+
+		var uniqueChar rune
+
+		for _, char := range left {
+			if strings.ContainsRune(right, char) {
+				uniqueChar = char
+				break
+			}
+		}
+
+		if uniqueChar == 0 {
+			for _, char := range left {
+				if strings.ContainsRune(right, char) {
+					uniqueChar = char
+					break
+				}
+			}
+		}
+
+        priority := func () (int) {
+            if unicode.IsLower(uniqueChar) {
+                return int(uniqueChar) - 96
+            } else {
+                return int(uniqueChar) - 38
+            }
+        } ()
+
+        result += priority
+	}
+
+	fmt.Println("Sum of the priorities is", result)
+	file.Close()
+}
---
+++ day03/part2.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+	"unicode"
+)
+
+func main() {
+	file_name := os.Args[1]
+	file, err := os.Open(file_name)
+
+	if err != nil {
+		fmt.Println("Error opening file!")
+	}
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+
+	var lines []string
+    result := 0
+
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	for i:= 0; i < len(lines); i+=3 {
+		first_elf := lines[i]
+		second_elf := lines[i+1]
+		third_elf := lines[i+2]
+
+		var uniqueChar rune
+
+		for _, char := range first_elf {
+			if strings.ContainsRune(second_elf, char) && strings.ContainsRune(third_elf, char) {
+				uniqueChar = char
+				break
+			}
+		}
+
+        result += func () (int) {
+            if unicode.IsLower(uniqueChar) {
+                return int(uniqueChar) - 96
+            } else {
+                return int(uniqueChar) - 38
+            }
+        } ()
+	}
+
+	fmt.Println("Sum of the priorities is", result)
+	file.Close()
+}
