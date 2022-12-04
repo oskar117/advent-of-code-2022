aff79460b2d30bec1147ef68a7d456bdff0c58c9
oskar117 <olek.konieczny@wp.pl>
2022-12-04 12:25:52 +0100

    day04

---
+++ day04/part1.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	file_name := os.Args[1]
+	file, err := os.Open(file_name)
+
+	if err != nil {
+		fmt.Println("Error opening file!")
+	}
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+
+	overlapping_pairs := 0
+
+	for scanner.Scan() {
+		a, b := split_pair(scanner.Text(), ",", func(s string) string { return s })
+		al, ah := split_pair(a, "-", better_atoi)
+		bl, bh := split_pair(b, "-", better_atoi)
+		if al <= bl && ah >= bh || al >= bl && ah <= bh {
+			overlapping_pairs++
+		}
+	}
+
+	fmt.Println(overlapping_pairs, "pairs overlap")
+	file.Close()
+}
+
+func split_pair[T any](text string, separator string, convert func(string) T) (T, T) {
+	splitted := strings.Split(text, separator)
+	return convert(splitted[0]), convert(splitted[1])
+}
+
+func better_atoi(val string) int {
+	num, _ := strconv.Atoi(val)
+	return num
+}
